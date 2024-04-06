75620ebe8e2dfe7c7d6f03f123bbc4795ecd054d
Angelin Sneha V <angelin.sneha@blueoptima.com>
2024-04-06 14:02:28 +0530

    Adding logger to error handler

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,8 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
 const BaseError = require("../errors/base.error");
+const logger = require("../../config/logger.config");
 
 function errorHandler(err, req, res, next) {
   if (err instanceof BaseError) {
+    logger.error(`Error Handler: Occurred with Status-code: ${err.statusCode}\n Message: ${err.message}`);
     return res.status(err.statusCode).json({
       success: false,
       message: err.message,
